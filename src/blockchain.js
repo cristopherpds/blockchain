a5766e73fc61f29ce7ea47fe16aec8fa9af98448
Cristopher Paiva <josuepaiva7@gmail.com>
2020-11-22 01:11:28 -0300

    cadena de bloques

--- src/blockchain.js
+++ src/blockchain.js
@@ -31,3 +31,48 @@ class Block{
         }
     }
 }
+
+class Blockchain {
+    constructor(){
+        this.blockchain = [this.startGenesisBlock()];
+        this.difficulty = 4;
+    }
+
+    startGenesisBlock(){
+        return new Block(0, '01/01/2020', 'Bloque inicial', '0');
+    }
+
+    obtainLatestBlock(){
+        return this.blockchain[this.blockchain.length -1];
+    }
+
+    addNewBlock(newBlock){
+
+        newBlock.precedingHash = this.obtainLatestBlock().hash;
+        //newBlock.hash = newBlock.computeHash();
+        newBlock.proofOfWork(this.difficulty);
+        this.blockchain.push(newBlock);
+
+    }
+
+
+    checkChainValidity(){
+        for (let i = 1; i < this.blockchain.length; i++) {
+            const currentBlock = this.blockchain[i];
+            const procedingBlock =this.blockchain[i -1];
+
+            if(currentBlock.hash !== currentBlock.computeHash()){
+                return false;
+            }
+            if(currentBlock.procedingHash !== procedingBlock.hash) return false;
+            
+        }
+        return true;
+    }
+}
+
+let newBlockchain = new Blockchain();
+
+
+
+console.log(JSON.stringify(newBlockchain, null, 4));
